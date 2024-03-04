1cf235eadda26b2937754b9fa59d8bce0be17b87
David Anton <davidanton037@gmail.com>
2024-03-04 15:32:32 -0500

    Add more projects & socials

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,28 +7,40 @@ interface Project {
   
   export const projects: Project[] = [
     {
-      title: "Owalds's Quest🎮",
-      description: "Oswald's Quest is an exciting RPG game currently under development using Java. Experience the immersive world and captivating storyline as you embark on a thrilling adventure.",
+      title: "Owalds's Quest - Game",
+      description: "An Open World 2D RPG game made with Java Swing libaries.",
       link: "https://github.com/KayTwenty/OswaldQuest",
       stack: "Java, Swing",
     },
     {
-      title: "Mac Repair Man🛠️",
-      description: "A website for an Apple technician repair shop located in Ontario.",
+      title: "Mac Repair Man - Website",
+      description: "A website for an Apple technician repair shop located in Kitchener Ontario.",
       link: "https://www.macrepairman.ca/",
       stack: "NodeJS, EJS, CSS3, Bootstrap, Docker",
     },
     {
-      title: "ExEditor⌨️",
-      description: "A Python based IDE with zen features & working language server.",
-      link: "https://exeditor.kaytwenty.com/",
-      stack: "Python, TKinter",
+      title: "LiveVHS - Web App",
+      description: "A livestreaming platform with zero censorship, powered by WebRTC and IPFS.",
+      link: "https://github.com/KayTwenty/LiveVHS",
+      stack: "NextJS, TypeScript, TailwindCSS, PrismaDB, WebRTC",
     },
     {
-      title: "SpotVerse🔊",
+      title: "SpotVerse - Discord Bot",
       description: "A Discord bot which can play high quality music into a Discord Voice call. It supports Youtube, Spotify & Soundcloud.",
       link: "https://spotverse.cc/",
       stack:
         "Discord.JS v14, TypeScript, Nextjs, TailwindCSS, MongoDB",
     },
+    {
+      title: "Midnight Cavern - Game",
+      description: "A 2D Top Down adventure game made with Unity2D and C#.",
+      link: "https://midnightcavern.kaytwenty.com/",
+      stack: "Unity2D, C#",
+    },
+    {
+      "title": "RuneEngine - Game Engine",
+      "description": "A 2D game engine made with Java and OpenGL.",
+      "link": "https://github.com/KayTwenty/RuneEngine",
+      "stack": "Java, Gradle, OpenGL, LWJGL"
+    }
   ];

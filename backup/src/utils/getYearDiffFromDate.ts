54fc61a475ca90f8b3e48d966e0e630e974f5e5a
David <davidanton037@gmail.com>
2025-11-26 17:51:19 -0500

    Remove backup and fix some bugs

--- backup/src/utils/getYearDiffFromDate.ts
+++
@@ -1,6 +0,0 @@
-export const getYearDiffFromDate = (date: string) => {
-    const dateTime = new Date(date);
-    return Math.abs(
-      new Date(Date.now() - dateTime.getTime()).getUTCFullYear() - 1970
-    );
-  };
--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,16 +1,9 @@
-import { useWindowScroll } from 'react-use/esm';
-
 export const GradientBackground = () => {
-  const { y } = useWindowScroll();
-
   return (
     <>
-      <div className='pointer-events-none absolute inset-0 overflow-hidden'>
+      <div className='pointer-events-none fixed inset-0 overflow-hidden h-screen w-screen'>
         <div
-          className="h-full bg-[url('https://res.cloudinary.com/delba/image/upload/h_500/bg_gradient_pfosr9')] bg-top bg-no-repeat opacity-[0.3] will-change-transform"
-          style={{
-            transform: `translateY(${Math.min(y / 3, 167)}px)`,
-          }}
+          className="h-full w-full bg-[url('https://res.cloudinary.com/delba/image/upload/h_500/bg_gradient_pfosr9')] bg-top bg-no-repeat bg-cover opacity-[0.3]"
         ></div>
       </div>
     </>

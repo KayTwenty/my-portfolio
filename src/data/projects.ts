178316116bbb248fc92b089dbff15ad41d15ac18
David <davidanton037@gmail.com>
2025-11-25 23:25:27 -0500

    Update projects on website

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,19 +8,25 @@ interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Oswald's Quest - Game",
-    description: "An Open World 2D RPG game made with Java Swing libaries.",
-    link: "https://github.com/KayTwenty/OswaldQuest",
-    stack: "Java, Swing, Tiled, Aseprite",
+    title: "Adrian Michalski Portfolio",
+    description: "A portfolio website for a professional photographer that showcases his work dynamically using a custom made content management system (CMS).",
+    link: "https://www.miskimedia.ca/",
+    stack: "Next.js, TailwindCSS, TypeScript, Prisma, SQLite",
     image:
-      "https://raw.githubusercontent.com/KayTwenty/OswaldQuest/main/.github/GitIcon.png",
+      "https://www.miskimedia.ca/logo.png",
   },
   {
-    title: "Mac Repair Man - Website",
+    title: "GG Central Website",
+    description: "A website for a local gaming cafe located in Kitchener Ontario. Features booking, full event backend system, tounament management, and more.",
+    link: "https://www.ggcentral.ca/",
+    stack: "Next.js, TypeScript, TailwindCSS, Prisma, PostgreSQL, C++",
+  },
+  {
+    title: "Mac Repair Man Website",
     description:
       "A website for an Apple technician repair shop located in Kitchener Ontario.",
     link: "https://www.macrepairman.ca/",
-    stack: "AstroJS, React, CSS3, Bootstrap, JQuery",
+    stack: "Astro.js, React, CSS, JavaScript",
     image: "https://avatars.githubusercontent.com/u/127373412?s=200&v=4",
   },
   {
@@ -32,12 +38,12 @@ export const projects: Project[] = [
     image: "https://raw.githubusercontent.com/KayTwenty/WhoAmEye/0dbbc489eb56957f929e321e43130cb700c16ec2/public/logo.svg",
   },
   {
-    title: "SpotVerse - Discord Bot",
-    description:
-      "A Discord bot which can play high quality music into a Discord Voice call. It supports Youtube, Spotify & Soundcloud.",
-    link: "https://spotverse.cc/",
-    stack: "Discord.JS v14, TypeScript, Nextjs, TailwindCSS, MongoDB",
-    image: "https://www.spotverse.cc/img/logo.png",
+    title: "Oswald's Quest - Game",
+    description: "An Open World 2D RPG game made with Java Swing libaries.",
+    link: "https://github.com/KayTwenty/OswaldQuest",
+    stack: "Java, Swing, Tiled, Aseprite",
+    image:
+      "https://raw.githubusercontent.com/KayTwenty/OswaldQuest/main/.github/GitIcon.png",
   },
   {
     title: "Midnight Cavern - Game",
